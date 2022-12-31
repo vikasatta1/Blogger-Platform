a229f22f8c1c937446d728b593f12841331d7125
Vika <Vikasatta1998@gmail.com>
2022-12-31 10:02:24 +0300

    BlogPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Routes, Route} from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
 import HomeBlogs from "./pages/HomeBlogs";
 import './scss/app.scss';
 import HomePosts from "./pages/HomePosts";
 import Header from "./components/header/Header";
 import Navigation from "./components/navigation/Navigation";
-import PostPage from "./pages/postPage";
+import BlogPage from "./pages/BlogPage";
 
 
 function App() {
@@ -15,12 +15,13 @@ function App() {
             <div className={"content"}>
                 <Navigation/>
                 <div className={'main'}>
-                    {/* <Routes>
+                     <Routes>
                         <Route path={'/'} element={<HomeBlogs/>}/>
                         <Route path={'/blogs'} element={<HomeBlogs/>}/>
                         <Route path={'/posts'} element={<HomePosts/>}/>
-                    </Routes>*/}
-                    <PostPage/>
+                    </Routes>
+                   {/* <PostPage/>
+                    <BlogPage/>*/}
                 </div>
             </div>
 
